d041ffdb51a3c9750eaaece22d6e6fd19812be34
GreenMK <mmmkanzawa@gmail.com>
2020-04-15 22:19:39 +0900

    スクロール画面表示
    投稿メッセージのスクロール画面表示

--- Gemfile
+++ Gemfile
@@ -65,5 +65,3 @@ gem 'pry-rails'
 gem 'carrierwave'
 
 gem 'mini_magick'
-
-gem 'jquery-rails'
--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,48 @@
-$(function(){
+$(function(){ 
+  function buildHTML(message){
+    if (message.image) {
+      var html =
+        `<div class="message">
+          <div class="message__post">
+            <div class="message__post__member-name">
+              ${message.user_name}
+            </div>
+            <div class="message__post__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="message__text">
+            <p class="lower-message__content">
+              ${message.content}
+            </p>
+          </div>
+          <img src=${message.image} >
+        </div>`
+      return html;
+    } else {
+      var html =
+        `<div class="message">
+          <div class="message__post">
+            <div class="message__post__member-name">
+              ${message.user_name}
+            </div>
+            <div class="message__post__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="message__text">
+            <p class="lower-message__content">
+              ${message.content}
+            </p>
+          </div>
+        </div>`
+      return html;
+    };
+  }
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
-    var url = $(this).attr('action');
+    var url = $(this).attr('action')
     $.ajax({
       url: url,
       type: "POST",
@@ -11,5 +51,11 @@ $(function(){
       processData: false,
       contentType: false
     })
-  });
+      .done(function(data){
+        var html = buildHTML(data);
+        $('.messages').append(html);
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+        $('form')[0].reset();
+      })
+  })
 });
